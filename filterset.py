9cee565a3cf2007c6699fe3f7ca1a943d9cc6697
tougshire <tougshire@tougshire.com>
2024-04-19 11:11:23 -0400

    Remove finds from Meta and define filters explicity as properties

--- filterset.py
+++ filterset.py
@@ -3,20 +3,33 @@ from .models import Person, Meeting, Membershipclass, Subcommittee
 from django.db import models
 from django import forms
 from django_filters_stoex.filters import CrossFieldSearchFilter
+from touglates.widgets import DropdownSelectMultiple
 
 
 class PersonFilter(django_filters.FilterSet):
     filterset_name = forms.CharField()
     combined_text_search = CrossFieldSearchFilter(
-        label="text search",
+        label="Name",
         field_name="name_last,name_first,name_middles,name_friendly",
         lookup_expr="icontains",
+        help_text="Any part of the person's name",
     )
+
     subcommittee__in = django_filters.ModelMultipleChoiceFilter(
         label="Sub-committee",
         field_name="submembership__subposition__subcommittee",
         queryset=Subcommittee.objects.all(),
+        help_text="Any of the subcomittees of which the person is a member",
+        widget=DropdownSelectMultiple(),
     )
+    # try:
+    #     subcommittee__in.widget = DropdownSelectMultiple()
+    #     print("tp244j755")
+    # except NameError:
+    #     try:
+    #         subcommittee__in.widget = touglates.DropdownSelectMultiple()
+    #     except NameError:
+    #         pass
 
     membershipclass = django_filters.ModelMultipleChoiceFilter(
         queryset=Membershipclass.objects.all(), widget=forms.CheckboxSelectMultiple()
@@ -38,24 +51,25 @@ class PersonFilter(django_filters.FilterSet):
 
     class Meta:
         model = Person
-        fields = [
-            "name_friendly",
-            "name_first",
-            "name_last",
-            "membershipclass",
-            "membershipclass__is_quorum_member",
-            "membership_date",
-            "districtprecinct",
-            "districtborough",
-            "districtstatehouse",
-            "districtstatesenate",
-            "districtcongress",
-            "primary_voice",
-            "primary_text",
-            "primary_email",
-            "voting_address",
-            "mailing_address",
-        ]
+        fields = []
+        # [
+        #     "name_friendly",
+        #     "name_first",
+        #     "name_last",
+        #     "membershipclass",
+        #     "membershipclass__is_quorum_member",
+        #     "membership_date",
+        #     "districtprecinct",
+        #     "districtborough",
+        #     "districtstatehouse",
+        #     "districtstatesenate",
+        #     "districtcongress",
+        #     "primary_voice",
+        #     "primary_text",
+        #     "primary_email",
+        #     "voting_address",
+        #     "mailing_address",
+        # ]
 
 
 class MeetingFilter(django_filters.FilterSet):
