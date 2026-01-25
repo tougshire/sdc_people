f13694b9bf2dfad9ae1941e3cafe49fd3818d855
tougshire <tougshire@tougshire.com>
2026-01-25 16:28:19 -0500

    add option to set length of attendance list

--- models.py
+++ models.py
@@ -513,14 +513,17 @@ class Person(models.Model):
                 meetings = meetings.filter(
                     meetingtype__name__in=settings.SDC_PEOPLE[
                         "meetingtypes_for_attendance"
-                    ]
+                ]
                 )
         attendance_binary = ""
+        if hasattr(settings, "SDC_PEOPLE"):
+            if 'attendance_list_length' in settings.SDC_PEOPLE:
+                meetings = meetings[:settings.SDC_PEOPLE['attendance_list_length']]
         for meeting in meetings:
             if meeting.attendance_set.filter(person=self).exists():
                 attendance_binary = attendance_binary + "Y,"
             else:
-                attendance_binary = attendance_binary + "n,"
+                attendance_binary = attendance_binary + "-,"
         return attendance_binary[:-1]
 
     def get_memberships(self):
